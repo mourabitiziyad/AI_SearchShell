3878221198f94dd89c5bcea84748f303947d4cfc
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2020-11-07 23:26:34 +0100

    yes

--- PegSoliaire.py
+++ PegSoliaire.py
@@ -4,6 +4,9 @@
 2: out of bound (the board is cross-shaped)
 
 '''
+
+
+
 test_state = [          [2, 2, 0, 1, 1, 2, 2], 
                         [2, 2, 1, 1, 0, 2, 2],
                         [1, 1, 0, 1, 0, 1, 1],
@@ -78,6 +81,40 @@ def all_possible_moves(state):
             if len(moves) > 0: # to be added before using possible_moves_from_index IN SEARCH STRATEGY
                 print(row, ",", col, ":", moves) # to be replaced by return (action, state) because successor function
 
+# def successor_function(state):
+#     locations = []
+#     default_state = state
+#     # all_moves = []
+#     for row in range(7):
+#         for col in range(7):
+#             locations.append([row, col])
+#             moves = possible_moves_from_index(state, [row, col])
+#     if len(moves) > 0: # to be added before using possible_moves_from_index IN SEARCH STRATEGY
+#         print(moves)
+#         for i in moves:
+#             print(i, i[0], i[1])
+#             state = default_state
+#             state[row][col], state[i[0]][i[1]] = state[i[0]][i[1]], state[row][col]
+#             if col == i[1] and row < i[0]:
+#                 state[row+1][col] = 0
+#             elif col == i[1] and row > i[0]:
+#                 state[row-1][col] = 0
+#             elif col < i[1] and row == i[0]:
+#                 state[row][col+1] = 0
+#             elif col > i[1] and row == i[0]:
+#                 state[row][col-1] = 0
+#             show_state(state)
+#             state = default_state
+#             print(state)
+
+def show_state(state):
+    file1 = open('possible_moves.txt', 'a') 
+    for i in state:
+        file1.writelines(str(i))
+        file1.write("\n")
+    for i in state:
+        print(i)
+    file1.write("\n")
 
                 
 
@@ -87,5 +124,5 @@ def all_possible_moves(state):
 # moves = possible_moves_from_index(initial_state, [3,1])
 # print(moves)
 
-all_possible_moves(test_state)
-
+all_possible_moves(initial_state)
+successor_function(initial_state)
