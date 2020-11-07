4d3b2b3460da0b737a51c6549e16e6b8c1ced3fc
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2020-11-08 00:39:05 +0100

    hamdullah

--- 8 puzzle .py
+++ 8 puzzle .py
@@ -6,13 +6,13 @@ N: Tile N
 
 '''
 
-initial_state = np.array( [   [7, 2, 4], 
+initial_state = np.array( [   			[7, 2, 4], 
 										[5, 0, 6],
 										[8, 3, 1]     ])
 
-goal_state = np.array([      [0, 1, 2], 
-                    [3, 4, 5],
-                    [6, 7, 8]     ])
+goal_state = np.array([      	[0, 1, 2], 
+                    			[3, 4, 5],
+                    			[6, 7, 8]     ])
 
 def goal_test(state, goal_state):
     if np.array_equal(state, goal_state):
