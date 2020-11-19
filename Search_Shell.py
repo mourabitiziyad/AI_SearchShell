30d61a19a4de51f6db0d25822381615428a9f2a6
Hamza Ait Hssayene <aithssayenehamza@gmail.com>
2020-11-19 20:23:55 +0100

    bfs edited

--- Search_Shell.py
+++ Search_Shell.py
@@ -8,8 +8,8 @@ from Eightpuzzle import successor_function, goal_test, heuristic
 
 # We should normally/probably take these as input and make them into the format we need
 initial_state = np.array([[3, 1, 2],
-                       [5, 0, 6],
-                       [7, 4, 8]])
+                          [6, 4, 5],
+                          [7, 0, 8]])
 
 goal_state = np.array([[0, 1, 2],
                        [3, 4, 5],
@@ -17,11 +17,11 @@ goal_state = np.array([[0, 1, 2],
 
 
 def uninformed_BFS(state, goal):
-    if(goal_test(state,goal)):
+    if(goal_test(state, goal)):
         return state
     q = Queue()
-    checked = np.array([])
-    np.append(checked,state)
+    checked = []
+    checked.append(state)
     nodes = successor_function(state)
     for n in nodes:
         q.put(n)
@@ -29,13 +29,16 @@ def uninformed_BFS(state, goal):
         n = q.get_nowait()
         if(goal_test(n, goal)):
             return n
-        # checked.append(n)
-        np.append(checked, n)
+        checked.append(n)
         nodes = successor_function(n)
         flag = False
         for i in nodes:
+            flag = False
             for c in checked:
-                if np.array_equal(i, c):
+                comparison = i == c
+                equal = comparison.all()
+                if equal:
+                    print("visited")
                     flag = True
                     break
             if flag == False:
@@ -44,4 +47,5 @@ def uninformed_BFS(state, goal):
                 print('H')
     return 0
 
-print(uninformed_BFS(initial_state,goal_state))
+
+print(uninformed_BFS(initial_state, goal_state))
