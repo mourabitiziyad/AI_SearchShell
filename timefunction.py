bdab38a63ad7f8878fd0c059765c73bc2d7f407a
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2020-11-20 22:49:59 +0100

    Final Commit

--- timefunction.py
+++
@@ -1,8 +0,0 @@
-import time
-start_time = time.time()
-
-'''
-Your entire code (or just the section that you want to time)
-
-'''
-print ("My program took", time.time() - start_time, "seconds to run")
