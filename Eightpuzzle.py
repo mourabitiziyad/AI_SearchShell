36364f96fdebaa63d6d73a2ba2ad3d9f398921b8
Hamza Ait Hssayene <aithssayenehamza@gmail.com>
2020-11-20 09:29:00 +0100

    Fixed 8 puzzle heuristic and added Greedy Best First Search

--- Eightpuzzle.py
+++ Eightpuzzle.py
@@ -1,25 +1,61 @@
 import numpy as np
+import copy
 
 '''
 0: blank
 N: Tile N
 
 '''
+# 3 + 1 + 1 + 1 + 1 + 1 + 2
 
-initial_state = np.array([[7, 2, 1],
-                          [5, 0, 6],
-                          [8, 3, 4]])
+initial_state = ([[7, 2, 1],
+                  [6, 0, 5],
+                  [3, 8, 4]])
 
-goal_state = np.array([[0, 1, 2],
-                       [3, 4, 5],
-                       [6, 7, 8]])
+goal_state = ([[0, 1, 2],
+               [3, 4, 5],
+               [6, 7, 8]])
 
 
 def goal_test(state, goal_state):
-    if np.array_equal(state, goal_state):
+    if state == goal_state:
         return True
     else:
         return False
+
+
+# def successor_function(state):
+#     outcomes = []
+#     for i in range(3):  # Locating the blank tile
+#         for j in range(3):
+#             if state[i][j] == 0:
+#                 brow = i
+#                 bcol = j
+#     if brow > 0:  # If blank tile can go up
+#         tempstate = state.copy()
+#         temp = tempstate[brow][bcol]
+#         tempstate[brow][bcol] = tempstate[brow-1][bcol]
+#         tempstate[brow-1][bcol] = temp
+#         outcomes.append(tempstate)
+#     if brow < 2:  # If blank tile can go down
+#         tempstate = state.copy()
+#         temp = tempstate[brow][bcol]
+#         tempstate[brow][bcol] = tempstate[brow+1][bcol]
+#         tempstate[brow+1][bcol] = temp
+#         outcomes.append(tempstate)
+#     if bcol > 0:  # If blank tile can go left
+#         tempstate = state.copy()
+#         temp = tempstate[brow][bcol]
+#         tempstate[brow][bcol] = tempstate[brow][bcol-1]
+#         tempstate[brow][bcol-1] = temp
+#         outcomes.append(tempstate)
+#     if bcol < 2:  # If blank tile can go right
+#         tempstate = state.copy()
+#         temp = tempstate[brow][bcol]
+#         tempstate[brow][bcol] = tempstate[brow][bcol+1]
+#         tempstate[brow][bcol+1] = temp
+#         outcomes.append(tempstate)
+#     return outcomes
 
 
 def successor_function(state):
@@ -30,25 +66,29 @@ def successor_function(state):
                 brow = i
                 bcol = j
     if brow > 0:  # If blank tile can go up
-        tempstate = state.copy()
+        # tempstate = state.copy()
+        tempstate = copy.deepcopy(state)
         temp = tempstate[brow][bcol]
         tempstate[brow][bcol] = tempstate[brow-1][bcol]
         tempstate[brow-1][bcol] = temp
         outcomes.append(tempstate)
     if brow < 2:  # If blank tile can go down
-        tempstate = state.copy()
+        # tempstate = state.copy()
+        tempstate = copy.deepcopy(state)
         temp = tempstate[brow][bcol]
         tempstate[brow][bcol] = tempstate[brow+1][bcol]
         tempstate[brow+1][bcol] = temp
         outcomes.append(tempstate)
     if bcol > 0:  # If blank tile can go left
-        tempstate = state.copy()
+        # tempstate = state.copy()
+        tempstate = copy.deepcopy(state)
         temp = tempstate[brow][bcol]
         tempstate[brow][bcol] = tempstate[brow][bcol-1]
         tempstate[brow][bcol-1] = temp
         outcomes.append(tempstate)
     if bcol < 2:  # If blank tile can go right
-        tempstate = state.copy()
+        # tempstate = state.copy()
+        tempstate = copy.deepcopy(state)
         temp = tempstate[brow][bcol]
         tempstate[brow][bcol] = tempstate[brow][bcol+1]
         tempstate[brow][bcol+1] = temp
@@ -60,25 +100,29 @@ def heuristic(state, goal):
     # Heuristic chosen is called linear conflicts
     # Manhattan Distance (usedin the overall heuristic)
     MD =0
+    i = 0
+    j = 0
     for i in range(3):
         for j in range(3):
             #Look up the tile in the goal state
             Gi = 0
             Gj = 0
             for Gi in range(3):
-                if state[i][j] in goal[Gi]:
+                if state[i][j] in goal[Gi] and state[i][j] != 0:
                     for Gj in range(3):
                         if state[i][j] == goal[Gi][Gj]:
+                            MD += abs(i - Gi) + abs(j - Gj)
                             break
-            MD += abs(i - Gi) + abs(j - Gj)
+            
     # Total heuristic is Manhattan Distance plus two times the liner conflicts
     LC = 0
     # We proceed row by row
+
     for i in range(3):
         conflicted = []
         # We count the number of conflicted tiles in each row
         for j in range(3):
-            if state[i][j] in goal[i]:
+            if state[i][j] in goal[i] and state[i][j] != 0:
                 conflicted.append(state[i][j])
         # if there is only one conflicting tile, that means no 2 tiles are conflicting
         # in which case we don't do anything.
@@ -87,21 +131,21 @@ def heuristic(state, goal):
             # Gi and Si are the indexes of the conflicting tile
             # in the goal state and our state repectively
         if len(conflicted) == 2:
-            G1 = np.where(goal[i] == conflicted[0])
-            G2 = np.where(goal[i] == conflicted[1])
-            S1 = np.where(state[i] == conflicted[0])
-            S2 = np.where(state[i] == conflicted[1])
+            G1 = goal[i].index(conflicted[0])
+            G2 = goal[i].index(conflicted[1])
+            S1 = state[i].index(conflicted[0])
+            S2 = state[i].index(conflicted[1])
             # We check if the tiles need to be reversed for them to reach their goal
             # that would mean that there is an LC (Linear Conflict)
             if (G1 > G2 and S1 < S2) or (G1 < G2 and S1 > S2):
                 LC += 1
         if len(conflicted) == 3:
-            G1 = np.where(goal[i] == conflicted[0])
-            G2 = np.where(goal[i] == conflicted[1])
-            G3 = np.where(goal[i] == conflicted[2])
-            S1 = np.where(state[i] == conflicted[0])
-            S2 = np.where(state[i] == conflicted[1])
-            S3 = np.where(state[i] == conflicted[2])
+            G1 = goal[i].index(conflicted[0])
+            G2 = goal[i].index(conflicted[1])
+            G3 = goal[i].index(conflicted[2])
+            S1 = state[i].index(conflicted[0])
+            S2 = state[i].index(conflicted[1])
+            S3 = state[i].index(conflicted[2])
             # Same logic for three tiles, but we have to test every combination
             if (G1 > G2 and S1 < S2) or (G1 < G2 and S1 > S2):
                 LC += 1
@@ -114,29 +158,34 @@ def heuristic(state, goal):
     col2 = [state[0][1], state[1][1], state[2][1]]
     col3 = [state[0][2], state[1][2], state[2][2]]
     state = [col1, col2, col3]
+    # We flip the goal too
+    col1 = [goal[0][0], goal[1][0], goal[2][0]]
+    col2 = [goal[0][1], goal[1][1], goal[2][1]]
+    col3 = [goal[0][2], goal[1][2], goal[2][2]]
+    goal = [col1, col2, col3]
     for i in range(3):
         conflicted = []
         for j in range(3):
-            if state[i][j] in goal[i]:
+            if state[i][j] in goal[i] and state[i][j] != 0:
                 conflicted.append(state[i][j])
         if len(conflicted) == 1:
             continue
         if len(conflicted) == 2:
-            G1 = np.where(goal[i] == conflicted[0])
-            G2 = np.where(goal[i] == conflicted[1])
-            S1 = np.where(state[i] == conflicted[0])
-            S2 = np.where(state[i] == conflicted[1])
+            G1 = goal[i].index(conflicted[0])
+            G2 = goal[i].index(conflicted[1])
+            S1 = state[i].index(conflicted[0])
+            S2 = state[i].index(conflicted[1])
             # We check if the tiles need to be reversed for them to reach their goal
             # that would mean that there is an LC (Linear Conflict)
             if (G1 > G2 and S1 < S2) or (G1 < G2 and S1 > S2):
                 LC += 1
         if len(conflicted) == 3:
-            G1 = np.where(goal[i] == conflicted[0])
-            G2 = np.where(goal[i] == conflicted[1])
-            G3 = np.where(goal[i] == conflicted[2])
-            S1 = np.where(state[i] == conflicted[0])
-            S2 = np.where(state[i] == conflicted[1])
-            S3 = np.where(state[i] == conflicted[2])
+            G1 = goal[i].index(conflicted[0])
+            G2 = goal[i].index(conflicted[1])
+            G3 = goal[i].index(conflicted[2])
+            S1 = state[i].index(conflicted[0])
+            S2 = state[i].index(conflicted[1])
+            S3 = state[i].index(conflicted[2])
             # Same logic for three tiles, but we have to test every combination
             if (G1 > G2 and S1 < S2) or (G1 < G2 and S1 > S2):
                 LC += 1
@@ -148,13 +197,13 @@ def heuristic(state, goal):
 
 
 # Printing initial node
-print("Initial State is: ")
-for i in range(3):
-    for j in range(3):
-        print(initial_state[i][j], end='')
-    print()
-print()
-print()
+# print("Initial State is: ")
+# for i in range(3):
+#     for j in range(3):
+#         print(initial_state[i][j], end='')
+#     print()
+# print()
+# print()
 
 
 # print(heuristic(initial_state, goal_state))
--- Search_Shell8puzzle.py
+++ Search_Shell8puzzle.py
@@ -1,5 +1,5 @@
 # function names are successor_function, goal_test, and heuristic
-from queue import Queue
+from queue import Queue, PriorityQueue
 import numpy as np
 from Eightpuzzle import successor_function, goal_test, heuristic
 # choice = eval(input("1 for 8 puzzle, stkhra you for the rest: "))
@@ -7,13 +7,15 @@ from Eightpuzzle import successor_function, goal_test, heuristic
 #     import Eightpuzzle
 
 # We should normally/probably take these as input and make them into the format we need
-initial_state = np.array([[3, 1, 2],
-                          [6, 4, 5],
-                          [7, 0, 8]])
+initial_state = [[1, 4, 2],
+                 [3, 7, 5],
+                 [6, 8, 0]]
 
-goal_state = np.array([[0, 1, 2],
-                       [3, 4, 5],
-                       [6, 7, 8]])
+goal_state = [[0, 1, 2],
+              [3, 4, 5],
+              [6, 7, 8]]
+
+
 
 
 def uninformed_BFS(state, goal):
@@ -35,10 +37,9 @@ def uninformed_BFS(state, goal):
         for i in nodes:
             flag = False
             for c in checked:
-                comparison = i == c
-                equal = comparison.all()
-                if equal:
-                    # print("visited")
+                # comparison = i == c
+                # equal = comparison.all()
+                if i == c:
                     flag = True
                     break
             if flag == False:
@@ -47,5 +48,39 @@ def uninformed_BFS(state, goal):
             #     print('H')
     return 0
 
+def greedyBFS(state, goal):
+    if(goal_test(state, goal)):
+        return state
+    q = PriorityQueue()
+    checked = []
+    checked.append(state)
+    nodes = successor_function(state)
+    for n in nodes:
+        q.put((heuristic(n, goal), n))
+    while q.empty() == False:
+        # print(q.queue)
+        n = q.get()
+        n = n[1]
+        # print(n)
+        # return
+        if(goal_test(n, goal)):
+            return n
+        checked.append(n)
+        nodes = successor_function(n)
+        flag = False
+        for i in nodes:
+            flag = False
+            for c in checked:
+                if i == c:
+                    flag = True
+                    break
+            if flag == False:
+                q.put((heuristic(i,goal), i))
+            # else:
+                # print("Visited")
+    return 0
 
-print(uninformed_BFS(initial_state, goal_state))
+
+# print(uninformed_BFS(initial_state, goal_state))
+print()
+print(greedyBFS(initial_state, goal_state))
