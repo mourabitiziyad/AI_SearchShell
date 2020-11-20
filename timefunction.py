8d489dacd41b12426889e721aea70cc140ce90bd
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2020-11-20 11:17:15 +0100

    code time, can be used to compare algo performance

---
+++ timefunction.py
@@ -0,0 +1,8 @@
+import time
+start_time = time.time()
+
+'''
+Your entire code (or just the section that you want to time)
+
+'''
+print ("My program took", time.time() - start_time, "seconds to run")
